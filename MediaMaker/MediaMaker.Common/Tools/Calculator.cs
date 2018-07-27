9301a029990a7e86c4c6cc96e01923a15b9968a3
Luke <lukecowley1987@gmail.com>
2018-07-27 13:55:51 +0100

    dummy classes for calculator

---
+++ MediaMaker/MediaMaker.Common/Tools/Calculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaMaker.Common.Tools
+{
+    public class Calculator
+    {
+    }
+}
---
+++ MediaMaker/MediaMaker.Common/Tools/Interfaces/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaMaker.Common.Tools.Interfaces
+{
+    public interface ICalculator
+    {
+    }
+}

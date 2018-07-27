b32e48b77f9190d34bfa6315f5c36251654f7f73
Luke <lukecowley1987@gmail.com>
2018-07-27 14:44:20 +0100

    calculator tests init

--- MediaMaker/MediaMaker.Common.Tests/Tools/CalculatorTests.cs
+++ MediaMaker/MediaMaker.Common.Tests/Tools/CalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using MediaMaker.Common.Tools;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,44 @@ namespace MediaMaker.Common.Tests.Tools
     [TestFixture]
     public class CalculatorTests
     {
+        private Calculator _calculator;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void Calculator_Add_Returns_Correct_Values()
+        {
+            //assemble
+            //act
+            //assert
+        }
+
+        [Test]
+        public void Calculator_Subtract_Returns_Correct_Value()
+        {
+            //assemble
+            //act
+            //assert
+        }
+
+        [Test]
+        public void Calculator_Divide_Returns_Correct_Value()
+        {
+            //assemble
+            //act
+            //assert
+        }
+
+        [Test]
+        public void Calculator_Multiply_Returns_Correct_Value()
+        {
+            //assemble
+            //act
+            //assert
+        }
     }
 }

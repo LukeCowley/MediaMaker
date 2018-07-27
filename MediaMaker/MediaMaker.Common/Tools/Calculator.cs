68a159b1fbafdb04b2a7d503adcc635bcfc55604
Luke <lukecowley1987@gmail.com>
2018-07-27 14:38:21 +0100

    stub methods assosiacted with calculator

--- MediaMaker/MediaMaker.Common/Tools/Calculator.cs
+++ MediaMaker/MediaMaker.Common/Tools/Calculator.cs
@@ -6,5 +6,24 @@ namespace MediaMaker.Common.Tools
 {
     public class Calculator : ICalculator
     {
+        public long Add(long firstValue, long secondValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(long numerator, long denominator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Multiply(long firstFactor, long secondFactor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Subtract(long subtrahend, long minuend)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MediaMaker/MediaMaker.Common/Tools/Interfaces/ICalculator.cs
+++ MediaMaker/MediaMaker.Common/Tools/Interfaces/ICalculator.cs
@@ -6,5 +6,32 @@ namespace MediaMaker.Common.Tools
 {
     public interface ICalculator
     {
+        /// <summary>
+        /// Adds two values
+        /// </summary>
+        /// <returns>Sum of the two values provided</returns>
+        long Add(long firstValue, long secondValue);
+
+        /// <summary>
+        /// Subtracts two values
+        /// </summary>
+        /// <param name="subtrahend">Initial value to be subtracted from</param>
+        /// <param name="minuend">Value to subtract</param>
+        /// <returns>Difference of the two values</returns>
+        long Subtract(long subtrahend, long minuend);
+
+        /// <summary>
+        /// Multiplies two values
+        /// </summary>
+        /// <returns>Product of the two inputs</returns>
+        long Multiply(long firstFactor, long secondFactor);
+
+        /// <summary>
+        /// Divides two values
+        /// </summary>
+        /// <param name="numerator">The initial value before division</param>
+        /// <param name="denominator">The value to divide by</param>
+        /// <returns></returns>
+        double Divide(long numerator, long denominator);
     }
 }

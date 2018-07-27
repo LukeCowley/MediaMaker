02c58573dfe2dadc13d1d92eb214f354d2765e31
Luke <lukecowley1987@gmail.com>
2018-07-27 15:21:18 +0100

    added docstring to concrete calculator class as inheritdoc wasn't working with dotnet standard despite the spec saying it does

--- MediaMaker/MediaMaker.Common/Tools/Calculator.cs
+++ MediaMaker/MediaMaker.Common/Tools/Calculator.cs
@@ -4,23 +4,44 @@ using System.Text;
 
 namespace MediaMaker.Common.Tools
 {
+    //TODO: inherit docstring from interface - inheritdoc.io giving issues with dotnet standard lib, should work!
     public class Calculator : ICalculator
     {
+        /// <summary>
+        /// Adds two values
+        /// </summary>
+        /// <returns>Sum of the two values provided</returns>
         public long Add(long firstValue, long secondValue)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Subtracts two values
+        /// </summary>
+        /// <param name="subtrahend">Initial value to be subtracted from</param>
+        /// <param name="minuend">Value to subtract</param>
+        /// <returns>Difference of the two values</returns>
         public long Subtract(long subtrahend, long minuend)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Multiplies two values
+        /// </summary>
+        /// <returns>Product of the two inputs</returns>
         public long Multiply(long firstFactor, long secondFactor)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Divides two values
+        /// </summary>
+        /// <param name="numerator">The initial value before division</param>
+        /// <param name="denominator">The value to divide by</param>
+        /// <returns></returns>
         public double Divide(long numerator, long denominator)
         {
             throw new NotImplementedException();
